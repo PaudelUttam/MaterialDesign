53c792787964bb7e66029ba6a5622d7f8a2fcf1b
Sachet Acharya <sachetacharya19@gmail.com>
2023-04-26 18:20:33 +0545

    navbar

--- src/app/public/client/client.component.ts
+++ src/app/public/client/client.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, AfterViewInit, OnDestroy, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-client',
-  templateUrl: './client.component.html',
-  styleUrls: ['./client.component.css']
+  selector: "app-client",
+  templateUrl: "./client.component.html",
+  styleUrls: ["./client.component.css"],
 })
-export class ClientComponent implements OnInit {
+export class ClientComponent implements AfterViewInit, OnDestroy {
+  
+  constructor() {}
 
-  constructor() { }
+  @ViewChild('myElement') myElement!: ElementRef;
+  fixedPosition: number = 0;
+  isFixedPositionReached: boolean = false;
+  element!: HTMLElement;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.element = this.myElement.nativeElement;
+    this.fixedPosition = this.element.getBoundingClientRect().top;
   }
 
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll = (event:any) => {
+    const currentPosition = window.pageYOffset;
+    if (currentPosition >= this.fixedPosition && !this.isFixedPositionReached) {
+      console.log('Fixed position reached');
+      this.isFixedPositionReached = true;
+      // Call your function here
+    } else if (currentPosition < this.fixedPosition && this.isFixedPositionReached) {
+      console.log('Fixed position not reached');
+      this.isFixedPositionReached = false;
+    }
+  }
 }
